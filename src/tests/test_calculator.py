9a74d44124ad46e21c4108383461d58f9dce9e47
G00dBunny <bradleysanchez28@gmail.com>
2025-09-13 20:01:12 -0400

    Update test_calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -1,6 +1,6 @@
 """
 Calculator app tests
-SPDX - License - Identifier: LGPL - 3.0 - or -later
+SPDX - License - Identifier: LGPL - 3.0 - or - later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
